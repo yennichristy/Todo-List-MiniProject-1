550a631e81f24aab9df213a1db87efe4a7aa0f79
Yenni Permatasari <yennikristianip@gmail.com>
2020-02-26 09:08:32 +0700

    [Yenni]-mini project

--- src/components/NewTask.js
+++ src/components/NewTask.js
@@ -17,35 +17,34 @@ const NewTask = () => {
 
   return (
     <div className="bigcon">
-      <div>
-        <div className="newcon">
-          <div className="exit">
-            <button className="exit--btn">X</button>
-          </div>
-          <form className="taskform">
-            <p className="taskform--title">Title:</p>
-            <input
-              className="areas"
-              type="text"
-              name="name"
-              onChange={change}
-              value={newTask.name}
-            ></input>
-            <p className="titlearea">Description:</p>
-            <textarea className="descarea" name="description"></textarea>
-            <p className="taskform--title">Due date:</p>
-            <input
-              className="areadate"
-              type="date"
-              name="dueDate"
-              onChange={change}
-              value={newTask.dueDate}
-            ></input>
-            <div className="submit">
-              <button className="submit--btn">Submit</button>
-            </div>
-          </form>
+      <div className="layer"></div>
+      <div className="newcon">
+        <div className="exit">
+          <button className="exit--btn">X</button>
         </div>
+        <form className="taskform">
+          <p className="taskform--title">Title:</p>
+          <input
+            className="areas"
+            type="text"
+            name="name"
+            onChange={change}
+            value={newTask.name}
+          ></input>
+          <p className="titlearea">Description:</p>
+          <textarea className="descarea" name="description"></textarea>
+          <p className="taskform--title">Due date:</p>
+          <input
+            className="areadate"
+            type="date"
+            name="dueDate"
+            onChange={change}
+            value={newTask.dueDate}
+          ></input>
+          <div className="submit">
+            <button className="submit--btn">Submit</button>
+          </div>
+        </form>
       </div>
     </div>
   );
--- src/components/Pages/DashBoardPage.js
+++ src/components/Pages/DashBoardPage.js
@@ -4,7 +4,7 @@ import "../../styles/DashBoard.scss";
 import picts from "../../pictures/sunflower.jpeg";
 import AddModal from "../Modals/AddModal";
 import EditModal from "../Modals/EditModal";
-// import { getAllTodos, deleteTodo, postNewTodos } from "../helpers/crudTodos";
+import DatePicker from "react-date-picker";
 
 const DashBoardPage = () => {
   let baseUrl = "https://miniprojectc.herokuapp.com/api/v1";
@@ -17,13 +17,23 @@ const DashBoardPage = () => {
     description: "",
     dueDate: ""
   });
-  const [complete, setComplete] = useState({
-    id: "",
-    completion: false
-  });
+
+  const [timDate, setTimdate] = useState(new Date());
+
+  const dateChange = date => {
+    setTimdate({ date });
+  };
+
   const [dataId, setDataId] = useState(null);
 
-  const change = () => {
+  const changeDataid = e => {
+    setDataId({
+      ...dataId,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const updateComplete = async id => {
     setCheckBox({
       checkBox: !checkBox
     });
@@ -55,50 +65,17 @@ const DashBoardPage = () => {
     setModalEdit(false);
   };
 
-  const getId = async id => {
-    console.log(id);
-    let token = localStorage.getItem("token");
+  //Filter Important & Completed ----------------------------------------------------------------------------------------------------------------------------
 
-    try {
-      const getIdRes = await fetch(`${baseUrl}/tasks/id?id=${id}`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-          Accept: "application/json"
-        }
-      });
-      const dataGetId = await getIdRes.json();
-      setDataId(dataGetId.data);
-      openModal();
-    } catch (error) {
-      console.log(error);
-    }
+  let filterIm = () => {
+    setTodos(todos.filter(item => item.importance === true));
   };
 
-  const updateComplete = async id => {
-    let token = localStorage.getItem("token");
-    const completeTask = {
-      id: complete.id,
-      completion: complete.completion
-    };
-
-    try {
-      let completeres = await fetch(`${baseUrl}/tasks/update/completion`, {
-        method: "PUT",
-        headers: {
-          "Content Type": "application/json",
-          Authorization: `Bearer ${token}`,
-          Accept: "application/json"
-        },
-        body: JSON.stringify({ completeTask })
-      });
-      const complete = await completeres.json();
-      console.log(complete);
-    } catch (error) {
-      console.log(error);
-    }
+  const filterCom = () => {
+    setTodos(todos.filter(item => item.completion === true));
   };
+
+  //Tasks New data -----------------------------------------------------------------------------------------------------------------------------------------
 
   const getAllTodos = async () => {
     let token = localStorage.getItem("token");
@@ -119,8 +96,103 @@ const DashBoardPage = () => {
     }
   };
 
-  const deleteTodo = async id => {
+  function formatDate(date) {
+    let dateOrder = new Date(date),
+      month = "" + (dateOrder.getMonth() + 1),
+      day = "" + dateOrder.getDate(),
+      year = dateOrder.getFullYear();
+
+    if (month.length < 2) month = "0" + month;
+    if (day.length < 2) day = "0" + day;
+
+    return [day, month, year].join("-");
+  }
+
+  const postNewTodos = async e => {
+    e.preventDefault();
+    let changeDate = formatDate(timDate.date);
+    let token = localStorage.getItem("token");
+    const newPost = {
+      name: post.name,
+      description: post.description,
+      dueDate: changeDate
+    };
+    try {
+      let postresponse = await fetch(`${baseUrl}/tasks`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+          Accept: "appplication/json"
+        },
+        body: JSON.stringify(newPost)
+      });
+      const data = await postresponse.json();
+      console.log("test", data);
+      await getAllTodos();
+      setPost("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  //Task Edit Data -------------------------------------------------------------------------------------------------------------------------------------------
+
+  const getId = async id => {
     console.log("id", id);
+    let token = localStorage.getItem("token");
+
+    try {
+      const getIdRes = await fetch(`${baseUrl}/tasks/id?id=${id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+          Accept: "application/json"
+        }
+      });
+      const dataGetId = await getIdRes.json();
+      setDataId(dataGetId.data);
+      console.log("data", dataId);
+      openEdit();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const postByid = async id => {
+    let token = localStorage.getItem("token");
+    const newTask = {
+      id: id,
+      name: dataId.name,
+      description: dataId.description,
+      dueDate: dataId.dueDate,
+      completion: true,
+      importance: true
+    };
+
+    console.log(newTask);
+    try {
+      let editRes = await fetch(`${baseUrl}/tasks`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+          Accept: "application/json"
+        },
+        body: JSON.stringify(newTask)
+      });
+      await editRes.json();
+      await getAllTodos();
+      closeEdit();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  //Task Delete -----------------------------------------------------------------------------------------------------------------------------------------------------
+
+  const deleteTodo = async id => {
     let token = localStorage.getItem("token");
     try {
       const delres = await fetch(`${baseUrl}/tasks`, {
@@ -139,26 +211,121 @@ const DashBoardPage = () => {
     }
   };
 
-  const postNewTodos = async e => {
-    e.preventDefault();
-    console.log("post");
+  const getUpdate = async id => {
     let token = localStorage.getItem("token");
-    const newPost = {
-      name: post.name,
-      description: post.description,
-      dueDate: "2020-03-24T17:27:16.586Z"
-    };
     try {
-      let postresponse = await fetch(`${baseUrl}/tasks`, {
-        method: "POST",
+      const imRes = await fetch(`${baseUrl}/tasks/id?id=${id}`, {
+        method: "GET",
         headers: {
           "Content-Type": "application/json",
           Authorization: `Bearer ${token}`,
-          Accept: "appplication/json"
-        },
-        body: JSON.stringify(newPost)
+          Accept: "application/json"
+        }
       });
-      await postresponse.json();
+      let data = await imRes.json();
+      setDataId(data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  //Task Filter Completed -------------------------------------------------------------------------------------------------------------------------------------------
+
+  const postCom = async id => {
+    await getUpdate(id);
+    let token = localStorage.getItem("token");
+    const completed = {
+      id: id,
+      completion: true
+    };
+    try {
+      const comRes = await fetch(`${baseUrl}/tasks/update/completion`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+          Accept: "application/json"
+        },
+        body: JSON.stringify(completed)
+      });
+      await comRes.json();
+      await getAllTodos();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const delCom = async id => {
+    await getUpdate(id);
+    let token = localStorage.getItem("token");
+    const completed = {
+      id: id,
+      completion: false
+    };
+    try {
+      const comRes = await fetch(`${baseUrl}/tasks/update/completion`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+          Accept: "application/json"
+        },
+        body: JSON.stringify(completed)
+      });
+      await comRes.json();
+      await getAllTodos();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  //Task Filter Important -------------------------------------------------------------------------------------------------------------------------------------------
+
+  const postIm = async id => {
+    await getUpdate(id);
+    let token = localStorage.getItem("token");
+    const important = {
+      id: id,
+      importance: true
+    };
+    try {
+      let impRes = await fetch(`${baseUrl}/tasks/update/importance`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+          Accept: "application/json"
+        },
+        body: JSON.stringify(important)
+      });
+      await impRes.json();
+      await getAllTodos();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  //Unimportant -----------------------------------------------------------------------------------------------------------------------------------------------
+  const delImp = async id => {
+    await getUpdate(id);
+    let token = localStorage.getItem("token");
+    const newTask = {
+      id: id,
+      importance: false
+    };
+    try {
+      let postimpRes = await fetch(`${baseUrl}/tasks/update/importance`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+          Accept: "application/json"
+        },
+        body: JSON.stringify(newTask)
+      });
+
+      await postimpRes.json();
+      console.log("change to false");
       await getAllTodos();
     } catch (error) {
       console.log(error);
@@ -172,15 +339,23 @@ const DashBoardPage = () => {
         type="checkbox"
         name="checkMe"
         value={checkBox}
-        onChange={change}
+        onChange={
+          item.completion ? () => delCom(item.id) : () => postCom(item.id)
+        }
       />
       <p className="title">{item.name}</p>
       <div className="icons">
         <div className="star">
-          <i className="fa far fa-star"></i>
+          <i
+            className="fa far fa-star"
+            style={{ color: item.importance ? "#4c9188" : "black" }}
+            onClick={
+              item.importance ? () => delImp(item._id) : () => postIm(item._id)
+            }
+          ></i>
         </div>
         <div className="editt">
-          <i className="fa fas fa-edit" onClick={openEdit}></i>
+          <i className="fa fas fa-edit" onClick={() => getId(item._id)}></i>
         </div>
         <div className="trash">
           <i
@@ -189,47 +364,6 @@ const DashBoardPage = () => {
           ></i>
         </div>
         {/* {false ? "mucul" : "gak muncul"} */}
-        {/* ini edit modal ----------------------------------------------------------------------------------------------------------------------------------- */}
-        <EditModal edit={modalEdit}>
-          <div className="bigcon">
-            <div>
-              <div className="newcon">
-                <div>
-                  <form className="taskform" onSubmit={postNewTodos}>
-                    <p className="taskform--title">Title:</p>
-                    <input
-                      className="areas"
-                      type="text"
-                      name="name"
-                      value={dataId && dataId.name}
-                      onChange={changePost}
-                    />
-                    <p className="titlearea">Description:</p>
-                    <input
-                      className="descarea"
-                      type="text"
-                      name="description"
-                      value={dataId && dataId.description}
-                      onChange={changePost}
-                    />
-                    <p className="taskform--title">Due date:</p>
-                    <input
-                      className="areadate"
-                      type="date"
-                      name="dueDate"
-                      value={dataId && dataId.dueDate}
-                      onChange={changePost}
-                    />
-                    <div className="submit">
-                      <button onClick={closeEdit}>Cancel</button>
-                      <button onClick={closeEdit}>Save Changes</button>
-                    </div>
-                  </form>
-                </div>
-              </div>
-            </div>
-          </div>
-        </EditModal>
       </div>
     </div>
   ));
@@ -269,12 +403,10 @@ const DashBoardPage = () => {
                     onChange={changePost}
                   />
                   <p className="taskform--title">Due date:</p>
-                  <input
-                    className="areadate"
-                    type="date"
-                    name="dueDate"
+                  <DatePicker
+                    selected={timDate.toString()}
+                    onChange={dateChange}
                     value={post.dueDate}
-                    onChange={changePost}
                   />
                   <div className="submit">
                     <button className="submit--btn">Submit</button>
@@ -286,6 +418,61 @@ const DashBoardPage = () => {
         </div>
       </AddModal>
 
+      {/* ini edit modal ----------------------------------------------------------------------------------------------------------------------------------- */}
+      <EditModal edit={modalEdit}>
+        <div className="bigcon">
+          <div>
+            <div className="newcon">
+              <div>
+                <div className="taskform">
+                  <p className="taskform--title">Title:</p>
+                  <input
+                    className="areas"
+                    type="text"
+                    name="name"
+                    value={dataId && dataId.name}
+                    onChange={changeDataid}
+                  />
+                  <p className="titlearea">Description:</p>
+                  <input
+                    className="descarea"
+                    type="text"
+                    name="description"
+                    value={dataId && dataId.description}
+                    onChange={changeDataid}
+                  />
+                  <p className="taskform--title">Due date:</p>
+                  <input
+                    className="areadate"
+                    type="date"
+                    dateFormat="dd/mm/yyyy"
+                    name="dueDate"
+                    value={dataId && dataId.dueDate}
+                    onChange={changeDataid}
+                  />
+                  {/* <DatePicker
+                    name="dueDate"
+                    value={dataId && dataId.dueDate}
+                    selected={timDate.toString()}
+                    onChange={dateChange}
+                  /> */}
+                  <div className="submit">
+                    <button className="submitbtn" onClick={closeEdit}>
+                      Cancel
+                    </button>
+                    <button
+                      className="submitbtn"
+                      onClick={() => postByid(dataId && dataId._id)}
+                    >
+                      Save Changes
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </EditModal>
       <div className="dashcon">
         <div className="dashtop">
           <h6 className="dashtop__left">Todos</h6>
@@ -300,26 +487,18 @@ const DashBoardPage = () => {
               <h5>Name</h5>
             </div>
             <div className="menu">
-              <h5>My day</h5>
-              <h5>Important</h5>
-              <h5>Completed</h5>
+              <button className="menubtn" onClick={getAllTodos}>
+                My days
+              </button>
+              <button className="menubtn" onClick={filterIm}>
+                Important
+              </button>
+              <button className="menubtn" onClick={filterCom}>
+                Completed
+              </button>
             </div>
           </div>
           <div className="dashbottom_right">
-            {/* <div>
-              <input
-                type="text"
-                value={post.name}
-                name="name"
-                onChange={changePost}
-              />
-              <input
-                type="text"
-                value={post.description}
-                name="description"
-                onChange={changePost}
-              />
-            </div> */}
             <div className="buttons">
               <button className="add" onClick={openModal}>
                 New Task
